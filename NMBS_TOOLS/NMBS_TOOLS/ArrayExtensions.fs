ef7c0779c61d4e5d5301fc279f7519cb6d1beb01
dariens <darien.shannon@gmail.com>
2017-06-30 18:12:06 -0700

    More

---
+++ NMBS_TOOLS/NMBS_TOOLS/ArrayExtensions.fs
@@ -0,0 +1,62 @@
+﻿namespace NMBS_Tools.ArrayExtensions
+
+module Array2D =
+
+    // Some generic functions for a single array that are required for the merge function but may also be helpful in some other cases:
+    
+    // arrayDimension = 0 for rows and 1 for columns,
+    // index represents the index of the row or column that you want 
+    let getSlice arrayDimension index (array: 'T[,]) = 
+        match arrayDimension with
+        | 0 -> array.[index..index, *] |> Seq.cast<'T> |> Seq.toArray
+        | 1 -> array.[*,index..index] |> Seq.cast<'T> |> Seq.toArray
+        | _ -> failwith "arrayDimension must be either 0 for rows and 1 for columns"
+        // getSlice 0 1 read "get the second row"
+        // getSlice 1 0 reads "get the first column"
+
+    // Converts an Array2D into a list of array's
+    // arrayDimension = 0 for rows and 1 for columns, 
+    let toArrayOfSlices arrayDimension (array : 'T[,]) : 'T[] list =
+        let iteratorDimension =
+            match arrayDimension with
+            | 0 -> 1
+            | 1 -> 0
+            | _ -> failwith "arrayDimension must be either 0 for rows and 1 for columns"
+        [for i = 0 to array.GetLength(arrayDimension) - 1 do
+            yield array |> getSlice arrayDimension i ]
+
+    let joinByRows (a1: 'a[,]) (a2: 'a[,]) =
+        let a1l1,a1l2,a2l1,a2l2 = (Array2D.length1 a1),(Array2D.length2 a1),(Array2D.length1 a2),(Array2D.length2 a2)
+        if a1l2 <> a2l2 then failwith "arrays have different column sizes"
+        let result = Array2D.zeroCreate (a1l1 + a2l1) a1l2
+        Array2D.blit a1 0 0 result 0 0 a1l1 a1l2
+        Array2D.blit a2 0 0 result a1l1 0 a2l1 a2l2
+        result
+
+    let joinByCols (a1: 'a[,]) (a2: 'a[,]) =
+        let a1l1,a1l2,a2l1,a2l2 = (Array2D.length1 a1),(Array2D.length2 a1),(Array2D.length1 a2),(Array2D.length2 a2)
+        if a1l1 <> a2l1 then failwith "arrays have different row sizes"
+        let result = Array2D.zeroCreate a1l1 (a1l2 + a2l2)
+        Array2D.blit a1 0 0 result 0 0 a1l1 a1l2
+        Array2D.blit a2 0 0 result 0 a1l2 a2l1 a2l2
+        result
+
+        // here joiner function must be Array2D.joinByRows or Array2D.joinByCols
+    let joinMany joiner (a: seq<'a[,]>)  = 
+        let arrays = a |> Array.ofSeq
+        if Array.length arrays = 0 then 
+            failwith "no arrays"
+        elif Array.length arrays = 1 then 
+            Array.head arrays
+        else
+            let rec doJoin acc arrays = 
+                if Array.length arrays = 0 then
+                    acc
+                elif Array.length arrays = 1 then
+                    joiner acc (Array.head arrays)
+                else
+                    let acc = joiner acc (Array.head arrays)
+                    doJoin acc (Array.tail arrays)
+            doJoin <|| (Array.head arrays, Array.tail arrays)
+            // or doJoin arrays.[0] arrays.[1..] 
+
--- NMBS_TOOLS/NMBS_TOOLS/BOM_Seismic_Seperation.fs
+++ NMBS_TOOLS/NMBS_TOOLS/BOM_Seismic_Seperation.fs
@@ -8,9 +8,11 @@ module Seperator =
     #endif
 
     open System
-    open NMBS_Tools.FrameExtensions
+    open System.IO
     open Microsoft.Office.Interop.Excel
     open System.Runtime.InteropServices
+    open NMBS_Tools.ArrayExtensions
+    
 
     type Load =
         {
@@ -26,26 +28,123 @@ module Seperator =
         Ref : string
         LoadCase : string
         }
+
+    let getAllInfo (reportPath : string ) (getInfoFunction : Workbook -> 'TOutput) =
+        let tempExcelApp = new Microsoft.Office.Interop.Excel.ApplicationClass(Visible = false)
+        let tempReportPath = System.IO.Path.GetTempFileName()
+        File.Delete(tempReportPath)
+        File.Copy(reportPath, tempReportPath)
+        let workbook = tempExcelApp.Workbooks.Open(tempReportPath)
+        let info = getInfoFunction workbook
+        workbook.Close()
+        Marshal.ReleaseComObject(workbook) |> ignore
+        System.GC.Collect() |> ignore
+        printfn "Finished processing %s." reportPath 
+        tempExcelApp.Quit()
+        Marshal.ReleaseComObject(tempExcelApp) |> ignore
+        System.GC.Collect() |> ignore
+        //printfn "Finished processing all files."
+        info
+   
+   
+    module Array2D =
+
+        // Some generic functions for a single array that are required for the merge function but may also be helpful in some other cases:
     
+        // arrayDimension = 0 for rows and 1 for columns,
+        // index represents the index of the row or column that you want 
+        let getSlice arrayDimension index (array: 'T[,]) = 
+            match arrayDimension with
+            | 0 -> array.[index..index, *] |> Seq.cast<'T> |> Seq.toArray
+            | 1 -> array.[*,index..index] |> Seq.cast<'T> |> Seq.toArray
+            | _ -> failwith "arrayDimension must be either 0 for rows and 1 for columns"
+            // getSlice 0 1 read "get the second row"
+            // getSlice 1 0 reads "get the first column"
+
+        // Converts an Array2D into a list of array's
+        // arrayDimension = 0 for rows and 1 for columns, 
+        let toArrayOfSlices arrayDimension (array : 'T[,]) : 'T[] list =
+            let iteratorDimension =
+                match arrayDimension with
+                | 0 -> 1
+                | 1 -> 0
+                | _ -> failwith "arrayDimension must be either 0 for rows and 1 for columns"
+            [for i = 0 to array.GetLength(arrayDimension) - 1 do
+                yield array |> getSlice arrayDimension i ]
+
+
+        // MergeType.
+        // Join is side-by-side and stack is on top of each-other.
+        type MergeType = Join=0 | Stack=1
+
+        let merge (array1 : 'T [,]) (array2 : 'T [,]) (mergeType : MergeType) =
+
+            // this is the dimension that is used in the array.GetLength function
+            // It will be 0 for rows and 1 for columns. 
+            let maintainedArrayDim =
+                match mergeType with
+                | MergeType.Join -> 0 // the number of rows remains the same when joining
+                | MergeType.Stack -> 1 // the number of columns remain the same when stacking
+                | _ -> failwith "mergeType enum value must be either 0 for Join, or 1 for Stack"
+
+            // Length of 'maintainedArrayDim' must be equal for both arrays
+            // i.e. for join, both arrays must have the same number of rows
+            // for stack, both arrays must have the same number of columns
+
+            let maintainedDimLength = array1.GetLength(maintainedArrayDim)
+            if array1.GetLength(maintainedArrayDim) <> array2.GetLength(maintainedArrayDim) then
+                failwith @"Both arrays must have the same number of columns for Array.Stack and same number of rows for Array.Join"
+
+            else
+                // create list of array's from rows for each array 
+                let array1Slices = array1 |> toArrayOfSlices 0  
+                let array2Slices = array2 |> toArrayOfSlices 0
+
+                // For Join type we need to do some funky business.
+                // For Stack type we just need to use List.append and create a array2D out of it. 
+                match mergeType with 
+                | MergeType.Join ->
+                    array2D [for i=0 to (maintainedDimLength - 1) do
+                                    yield Array.concat [array1Slices.[i] ; array2Slices.[i]]]
+                | MergeType.Stack ->
+                    array2D (List.append array1Slices array2Slices)
+                | _ -> failwith "mergeType enum value must be either 0 for Join, or 1 for Stack"
+ 
     let getAllLoadsAsArray (bom: Workbook) =
         let arrayList =
             [for sheet in bom.Worksheets do
                 let sheet = (sheet :?> Worksheet)
-                yield sheet.Range("A14","M55").Value2 :?> obj array]
+                if sheet.Name.Contains("L (") then
+                    yield sheet.Range("A14","M55").Value2 :?> obj [,]]
+
+    
+
+        //Array2D.joinMany (Array2D.joinByCols) arrayList
+        //[List.fold (Array2D.joinByCols) (array2D [[];[]]) arrayList]
         arrayList
-        //let rec combineArrays arrayList =
-        //    match arrayList with
-        //    | [] -> array2D [[];[]]
-        //    | head :: tail -> head |> Array2D.append (combineArrays tail)   /// Array2D.append ??? createa  function for this
-        //combineArrays arrayList
 
-    let array1 = array2D [[111;112]; [121;122]]
-    let array2 = array2D [[211;212]; [221;222]]
-    let arrayList = [array1;array2]
 
-    module Array2D =
-        let append (array1 : 'T [,]) (array2 : 'T [,]) =
-            sprintf "temp"
+    let excelPaths = @"C:\Users\darien.shannon\Desktop\4317-0092 Joist BOMs-For_Import_06-28-17.xlsm"
+
+    let loads = getAllInfo excelPaths getAllLoadsAsArray
+
+    
+    for load in loads do
+        printfn "Rows: %i, Cols: %i" (load.GetLength(0)) (load.GetLength(1))
+
+
+
+
+
+                       
+
+
+
+    
+
+
+
+    
 
     
 
